8879d6babb4dcf4c28c539e552d1189a2b0949af
Htoo Eaindra <htooeaindralin7@gmail.com>
2024-07-18 14:02:42 +0900

    Card Two Examples

--- src/index.js
+++ src/index.js
@@ -2,37 +2,48 @@ import React, { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const card1 = {
+  image: "https://via.placeholder.com/150/92c952",
+  title: "card Title One",
+  description: "card description",
+};
+
+const card2 = {
+  image: "https://via.placeholder.com/150/771796",
+  title: "card Title Two",
+  description: "card description",
+};
+
 // JSX
 function CardList() {
   return (
     <main className="card-list">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card
+        image={card1.image}
+        title={card1.title}
+        description={card1.description}
+      />
+      <Card
+        image={card2.image}
+        title={card2.title}
+        description={card2.description}
+      />
     </main>
   );
 }
 
 //const title = "Card Title";
 
-function Card() {
-  const title = "Card Title";
+function Card(props) {
+  console.log(props)
   return (
     <section className="card">
-      <img src="https://via.placeholder.com/150/92c952" alt="card-image" />
-      <h1 style={{marginTop:"1rem"}}>{title}</h1>
-      <Description />
+      <img src={props.image} alt="card-image" />
+      <h1 style={{marginTop:"1rem"}}>{props.title}</h1>
+      <p>{props.description}</p>     
     </section>
-  );
-  function Description() {
-    return (
-      <p style={{marginTop:"1rem"}}>
-        A paragraph is a series.
-      </p>
-    );
-  }
-}
+  ); 
+} 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<CardList />);
