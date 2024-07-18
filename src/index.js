770e71b2f78085be5f1e7f74f3041c8e200e56a6
Htoo Eaindra <htooeaindralin7@gmail.com>
2024-07-18 21:03:30 +0900

    Rendering Lists in React

--- src/index.js
+++ src/index.js
@@ -2,48 +2,50 @@ import React, { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const card1 = {
-  image: "https://via.placeholder.com/150/92c952",
-  title: "card Title One",
-  description: "card description",
-};
+const cards = [
+  {
+    id: 1,
+    image: "https://via.placeholder.com/150/92c952",
+    title: "card Title One",
+    description: "First description",
+  },
+  {
+    id: 2,
+    image: "https://via.placeholder.com/150/771796",
+    title: "card Title Two",
+    description: "Second description",
+  },
+  {
+    id: 3,
+    image: "https://via.placeholder.com/150/92c952",
+    title: "card Title Third",
+    description: "Third description",
+  },
+];
 
-const card2 = {
-  image: "https://via.placeholder.com/150/771796",
-  title: "card Title Two",
-  description: "card description",
-};
+///const fruits =["orange","apple","banana"];
+
+//const newCards = cards.map((card) => console.log(card));
+
+///const newFruits = fruits.map((fruit) => <h1>{fruit}</h1>);
 
 // JSX
 function CardList() {
-  return (
-    <main className="card-list">
-      <Card
-        image={card1.image}
-        title={card1.title}
-        description={card1.description}
-      >
-        <p>This is my paragraph for the first card.</p>
-
-      </Card>
-      <Card
-        image={card2.image}
-        title={card2.title}
-        description={card2.description}
-      />
-    </main>
-  );
+  return <main className="card list">{cards.map ((card) => {
+    const {img,title,description} = card;
+    return <Card key={card.id} {...card} />;
+  })}</main>;
 }
 
 //const title = "Card Title";
 
-function Card({image, title, description, children}) {  
+function Card(props) {    
+  const { image, title, description } = props;
   return (
     <section className="card">
       <img src={image} alt="card-image" />
       <h1 style={{ marginTop: "1rem" }}>{title}</h1>
       <p>{description}</p>
-      {children}
     </section>
   );
 } 
