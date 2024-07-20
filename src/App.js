167f0cc261e6da78f39dbe982201226b6bdad7b6
Htoo Eaindra <htooeaindralin7@gmail.com>
2024-07-20 22:55:00 +0900

    4-useUpdateEffect

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import ToggleComponent from './1-UseToggle/ToggleComponent';
 import TimeoutComponent from './2-UseTimeOut/TimeoutComponent';
 import DebounceComponent from './3-useDebounce/DebounceComponent';
-
+import UpdateEffectComponent from './4-useUpdateEffect/UpdateEffectComponent';
 
 function App() {
     // return <ToggleComponent />
     // return <TimeoutComponent />;
-    return <DebounceComponent />;
+    //return <DebounceComponent />;
+    return <UpdateEffectComponent />;
   
 }
 
