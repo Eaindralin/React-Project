3ab250204fd20b97b8968d5eaafb0c6a91bbb6aa
Htoo Eaindra <htooeaindralin7@gmail.com>
2024-07-20 12:28:28 +0900

    3-useDebounce

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 
 import ToggleComponent from './1-UseToggle/ToggleComponent';
 import TimeoutComponent from './2-UseTimeOut/TimeoutComponent';
+import DebounceComponent from './3-useDebounce/DebounceComponent';
 
 
 function App() {
     // return <ToggleComponent />
-    return <TimeoutComponent />;
+    // return <TimeoutComponent />;
+    return <DebounceComponent />;
   
 }
 
