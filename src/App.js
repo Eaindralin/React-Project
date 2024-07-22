663094e6b050bff7e30c87964a6320a0113b0c19
Htoo Eaindra <htooeaindralin7@gmail.com>
2024-07-22 10:02:03 +0900

    11-useScript

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PreviousComponent from './6-usePrevious/PreviousComponent';
 import StateWithHistoryComponent from './7-useStateWithHistory/StateWithHistoryComponent';
 import StorageComponent from './8-useStorage/StorageComponent';
 import AsyncComponent from './9-useAsync/AsyncComponent';
+import ScriptComponent from './11-useScript/ScriptComponent';
 
 function App() {
     // return <ToggleComponent />
@@ -19,7 +20,8 @@ function App() {
    // return <ArrayComponent />;
    //return <StateWithHistoryComponent/>
    //return <StorageComponent/>
-   return <AsyncComponent/>;
+   //return <AsyncComponent/>;
+   return <ScriptComponent />;
 }
 
 export default App;
